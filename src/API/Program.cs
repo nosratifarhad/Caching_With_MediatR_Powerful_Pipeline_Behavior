001b67e9f52b66f789f62f6aac3c281ff610c7fc
nosratifarhad01@gmail.com <nosratifarhad01@gmail.com>
2024-05-22 21:02:29 +0330

    --add implements

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,11 +1,33 @@
+using Application.Common.CacheCommon;
+using MediatR;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDistributedMemoryCache();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region [ Options ]
+
+builder.Services.Configure<CacheSettings>(builder.Configuration.GetSection("CacheSettings"));
+
+#endregion [ Options ]
+
+#region [ MediatR ]
+
+builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+
+#endregion [ MediatR ]
+
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheBehavior<,>));
+
 
 var app = builder.Build();
 
---
+++ src/Application/Features/Users/ViewModel/UserBriefViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Features.Users.ViewModel;
+
+public class UserBriefViewModel
+{
+    public int UserId { get; set; }
+
+    public string UserName { get; set; }
+}
